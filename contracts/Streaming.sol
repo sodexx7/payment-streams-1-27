b765501510a9203d9b7cda6b03e1b176d6bbaa6e
tony <sodexx7@gmail.com>
2022-06-15 17:42:59 +0800

    basic funciton check and add function cancelStream 1. createStream change the deposit equals the msg.value in the beginning.
         todo is OK?
    
    2. withdrawFromStream funciton check
    
        check states:
    
        1. streamID check
        2. delete the modifier onlySenderOrRecipient,add require:only  recipient can call.
    
         change  the states
    
        1. change the balance
        2. chaneg the stream startTime to the current block.timestamp.
    
        execute
    
        1. call funciton return bool value ，and apply require
        2. change the order：call funciton before balance change
    
    3. add cancelStream
    
        add event CreateStream
    
        check states:
        // 1）validate streamId
        // 2) validate sender or recipent
        // 3)check stream cancel or end? through by the banlance( sender and recipient)
    
        change the states:
    
        streams[streamId].balance = 0;
    
        execute
    
        two call funciton transfer value. if the corrospending address has value,then transfer.
    
        todo two funciton batch execute Is it OK?
    
    4. add modifier   onlyValidateSreamId ,apply to
    
       balanceOf,
    
       elapsedTimeFor,
    
       elapsedTimeFor,
    
       withdrawFromStream,g
    
       getStream,
    
       cancelStream

--- contracts/Streaming.sol
+++ contracts/Streaming.sol
@@ -2,18 +2,22 @@
 pragma solidity ^0.8.4;
 
 contract Streaming {
-
     address public owner;
-    
+
     mapping(uint256 => Stream) private streams;
-    
+
     uint64 public streamIdCounter;
-    
+
     modifier onlySenderOrRecipient(uint256 streamId) {
         require(
-            msg.sender == streams[streamId].sender || msg.sender == streams[streamId].recipient,
+            msg.sender == streams[streamId].sender ||
+                msg.sender == streams[streamId].recipient,
             "caller is not the sender or the recipient of the stream"
         );
+        _;
+    }
+    modifier onlyValidateSreamId(uint256 streamId) {
+        require(streamId <= streamIdCounter, "stream does not exist");
         _;
     }
 
@@ -26,7 +30,7 @@ contract Streaming {
         uint256 rate;
         uint256 balance;
     }
-    
+
     event CreateStream(
         uint256 indexed streamId,
         address indexed sender,
@@ -36,55 +40,85 @@ contract Streaming {
         uint256 stopTime
     );
 
-    event WithdrawFromStream(uint256 indexed streamId, address indexed recipient);
-    
+    event WithdrawFromStream(
+        uint256 indexed streamId,
+        address indexed recipient
+    );
+
+    event CancelStream(
+        uint256 indexed streamId,
+        address indexed sender,
+        address indexed recipient,
+        uint256 vestedAmount,
+        uint256 remainingAmount
+    );
+
     constructor() {
-        owner  = msg.sender;
+        owner = msg.sender;
     }
-    
+
     function createStream(
-            address recipient,
-            uint256 deposit,
-            uint256 startTime,
-            uint256 stopTime
+        address recipient,
+        uint256 deposit,
+        uint256 startTime,
+        uint256 stopTime
     ) public payable returns (uint256 streamId) {
-        
+        deposit = msg.value;
         require(recipient != address(0x00), "Stream to the zero address");
         require(recipient != address(this), "Stream to the contract itself");
         require(recipient != msg.sender, "Stream to the caller");
+
         require(deposit > 0, "Deposit is equal to zero");
-        require(startTime >= block.timestamp, "Start time before block timestamp");
-        
+        require(
+            startTime >= block.timestamp,
+            "Start time before block timestamp"
+        );
+
         uint256 duration = stopTime - startTime;
-        
+
         require(deposit >= duration, "Deposit smaller than duration");
-        require(deposit % duration == 0, "Deposit is not a multiple of time delta");
-        
+        require(
+            deposit % duration == 0,
+            "Deposit is not a multiple of time delta"
+        );
+
         streamIdCounter += 1;
         uint256 currentStreamId = streamIdCounter;
-        
+
         // Rate Per second
         uint256 rate = deposit / duration;
-        
+
         streams[currentStreamId] = Stream({
-           balance: deposit,
-           deposit: deposit,
-           rate: rate,
-           recipient: recipient,
-           sender: msg.sender,
-           startTime: startTime,
-           stopTime: stopTime
+            balance: deposit,
+            deposit: deposit,
+            rate: rate,
+            recipient: recipient,
+            sender: msg.sender,
+            startTime: startTime,
+            stopTime: stopTime
         });
-        
-        emit CreateStream(currentStreamId, msg.sender, recipient, deposit, startTime, stopTime);
+
+        emit CreateStream(
+            currentStreamId,
+            msg.sender,
+            recipient,
+            deposit,
+            startTime,
+            stopTime
+        );
         return currentStreamId;
     }
-    
-    function balanceOf( uint256 streamId, address who)  public view returns (uint256 balance) {
+
+    function balanceOf(uint256 streamId, address who)
+        public
+        view
+        onlyValidateSreamId(streamId)
+        returns (uint256 balance)
+    {
         Stream memory stream = streams[streamId];
         uint256 elapsedTime = elapsedTimeFor(streamId);
         uint256 due = elapsedTime * stream.rate;
-        
+
         if (who == stream.recipient) {
             return due;
         } else if (who == stream.sender) {
@@ -93,37 +127,53 @@ contract Streaming {
             return 0;
         }
     }
-        
-        
-    function elapsedTimeFor(uint256 streamId) private view returns (uint256 delta) {
+
+    function elapsedTimeFor(uint256 streamId)
+        private
+        view
+        onlyValidateSreamId(streamId)
+        returns (uint256 delta)
+    {
         Stream memory stream = streams[streamId];
-        
+
         // Before the start of the stream
         if (block.timestamp <= stream.startTime) return 0;
-        
+
         // During the stream
-        if (block.timestamp < stream.stopTime) return block.timestamp - stream.startTime;
-        
+        if (block.timestamp < stream.stopTime)
+            return block.timestamp - stream.startTime;
+
         // After the end of the stream
         return stream.stopTime - stream.startTime;
     }
-    
-    function withdrawFromStream(
-            uint256 streamId
-    )  public 
-        onlySenderOrRecipient(streamId) {
-        uint256 balance = balanceOf(streamId, streams[streamId].recipient);
+
+    function withdrawFromStream(uint256 streamId)
+        public
+        onlyValidateSreamId(streamId)
+    {
+        // check  Recipient
+        require(
+            msg.sender == streams[streamId].recipient,
+            "The caller should be recipient"
+        );
+
+        uint256 balance = balanceOf(streamId, msg.sender);
         require(balance > 0, "Available balance is 0");
-        
-        payable(streams[streamId].recipient).call{value: balance}("");
-        streams[streamId].balance = 0;
-        
+
+        streams[streamId].balance -= balance;
+        streams[streamId].startTime = block.timestamp;
+        (bool success, ) = payable(streams[streamId].recipient).call{
+            value: balance
+        }("");
+        require(success);
+
         emit WithdrawFromStream(streamId, streams[streamId].recipient);
     }
 
     function getStream(uint256 streamId)
         external
         view
+        onlyValidateSreamId(streamId)
         returns (
             address sender,
             address recipient,
@@ -140,5 +190,49 @@ contract Streaming {
         stopTime = streams[streamId].stopTime;
         rate = streams[streamId].rate;
     }
-    
+
+    function cancelStream(uint256 streamId)
+        external
+        onlyValidateSreamId(streamId)
+        onlySenderOrRecipient(streamId)
+    {
+        // check state
+        // 1）validate streamId
+        // 2) validate sender or recipent
+        // 3)check stream cancel or end? through by the banlance( sender and recipient)
+
+        uint256 vestedAmount = balanceOf(streamId, streams[streamId].sender);
+
+        uint256 remainingAmount = balanceOf(
+            streamId,
+            streams[streamId].recipient
+        );
+
+        if (vestedAmount == 0 && remainingAmount == 0) {
+            revert("The stream has been cancel or end");
+        }
+
+        streams[streamId].balance = 0;
+
+        if (remainingAmount > 0) {
+            (bool success, ) = payable(streams[streamId].recipient).call{
+                value: vestedAmount
+            }("");
+            require(success);
+        }
+        if (remainingAmount > 0) {
+            (bool success, ) = payable(streams[streamId].sender).call{
+                value: remainingAmount
+            }("");
+            require(success);
+        }
+
+        emit CancelStream(
+            streamId,
+            streams[streamId].sender,
+            streams[streamId].recipient,
+            vestedAmount,
+            remainingAmount
+        );
+    }
 }
